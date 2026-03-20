24be064de854258f137bd2ecf9009e9e2d9ce805
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-03-20 19:31:49 +0900

    Feat/#73/date 타입 추가 설정

--- src/types/gantt.ts
+++ src/types/gantt.ts
@@ -8,12 +8,14 @@ export type ApiPage = {
   url: string
   title: string
   date?: DateRange
+  completedDate?: DateRange
   status: string
   isNew?: boolean
 }
 
 export type Page = ApiPage & {
   originalDate?: DateRange
+  originalCompletedDate?: DateRange
   originalTitle?: string
   originalStatus?: string
 }
